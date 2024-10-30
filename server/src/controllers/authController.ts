f954695cdaae4576976a6543f8b26a7a1f92cb0e
rosa <rosahadi19@gmail.com>
2024-10-30 20:35:56 +0300

    feat: add validation functions for email, password, and name
    - Implemented `validateEmail` to check for valid email format and existing users during signup
    - Added `validatePassword` to ensure password requirements and confirmation matching
    - Created `validateName` to ensure the name is provided

---
+++ server/src/controllers/authController.ts
@@ -0,0 +1,3 @@
+import catchAsync from '../utils/catchAsync';
+
+export const signup = catchAsync(async (req, res, next) => {});
---
+++ server/src/utils/validation.ts
@@ -0,0 +1,49 @@
+import { Model } from 'mongoose';
+import { UserDocument } from '../types';
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+type UserModel = Model<UserDocument>;
+
+exports.validateEmail = async (
+  email: string,
+  User: UserModel,
+  isSignup = true,
+) => {
+  let error;
+  if (!email) {
+    error = 'Please provide your email.';
+  } else if (!emailRegex.test(email)) {
+    error = 'Please provide a valid email format.';
+  } else if (isSignup) {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      error = 'This email is already in use. Please use another email.';
+    }
+  }
+  return error;
+};
+
+exports.validatePassword = (password: string, passwordConfirm: string) => {
+  const errors: { password?: string; passwordConfirm?: string } = {};
+
+  if (!password) {
+    errors.password = 'Please provide a password.';
+  } else if (password.length < 8) {
+    errors.password = 'Password must be at least 8 characters long.';
+  }
+
+  if (!passwordConfirm) {
+    errors.passwordConfirm = 'Please confirm your password.';
+  } else if (password !== passwordConfirm) {
+    errors.passwordConfirm = 'Passwords do not match.';
+  }
+
+  return errors;
+};
+
+exports.validateName = (name: string) => {
+  if (!name) {
+    return 'Please provide your name.';
+  }
+};
