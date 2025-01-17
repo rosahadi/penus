226d6bfcf74a64c11c85d9e2a01c79e5dddeb195
rosa <rosahadi19@gmail.com>
2025-01-17 13:33:21 +0300

    feat: refactor password change form to use ShadCN form components and integrate password mutation for update functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router';
+import { Navigate, Route, Routes } from 'react-router';
 import Nav from './components/nav/Nav';
 import Home from './pages/Home';
 import ResetPassword from './pages/ResetPassword';
@@ -57,7 +57,7 @@ function App() {
           />
           {/* */}
 
-          {/* <Route path="*" element={<Navigate to="/" />} /> */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </main>
     </>
--- client/src/components/settingsPage/PasswordModal.tsx
+++ client/src/components/settingsPage/PasswordModal.tsx
@@ -6,45 +6,135 @@ import {
 } from '@/components/ui/dialog';
 import { buttonStyles } from '@/utils/buttonStyles';
 import { Button } from '../Button';
+import { useMutation } from '@tanstack/react-query';
+import { updateCurrentPassword } from '@/api/user';
+import { useForm } from 'react-hook-form';
+import { UpdatePasswordFormData, UpdatePasswordFormError } from '@/types';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { useState } from 'react';
 
 export function PasswordModal() {
+  const [errors, setErrors] = useState<UpdatePasswordFormError | null>(null);
+
+  const form = useForm({
+    defaultValues: {
+      passwordCurrent: '',
+      password: '',
+      passwordConfirm: '',
+    },
+  });
+
+  const mutatePassword = useMutation({
+    mutationFn: updateCurrentPassword,
+    onSuccess: () => {
+      form.reset();
+    },
+    onError: (error: UpdatePasswordFormError) => {
+      setErrors(error);
+    },
+  });
+
+  const onSubmit = (data: UpdatePasswordFormData) => {
+    mutatePassword.mutate({
+      passwordCurrent: data.passwordCurrent,
+      password: data.password,
+      passwordConfirm: data.passwordConfirm,
+    });
+  };
+
   return (
     <DialogContent>
       <DialogHeader>
         <DialogTitle>Change Password</DialogTitle>
       </DialogHeader>
-      <form className="space-y-8">
-        <Input
-          type="password"
-          placeholder="Current Password"
-          className="bg-inputBg border-inputBorder focus:border-inputFocus"
-        />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            name="passwordCurrent"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Current Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter current password"
+                    {...field}
+                  />
+                </FormControl>
+                {errors?.passwordCurrent && (
+                  <FormMessage>{errors.passwordCurrent}</FormMessage>
+                )}
+              </FormItem>
+            )}
+          />
 
-        <Input
-          type="password"
-          placeholder="New Password"
-          className="bg-inputBg border-inputBorder focus:border-inputFocus"
-        />
+          <FormField
+            name="password"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>New Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter new password"
+                    {...field}
+                  />
+                </FormControl>
+                {errors?.password && (
+                  <FormMessage>{errors.password}</FormMessage>
+                )}
+              </FormItem>
+            )}
+          />
 
-        <Input
-          type="password"
-          placeholder="Confirm New Password"
-          className="bg-inputBg border-inputBorder focus:border-inputFocus"
-        />
+          <FormField
+            name="passwordConfirm"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm New Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Confirm new password"
+                    {...field}
+                  />
+                </FormControl>
+                {errors?.passwordConfirm && (
+                  <FormMessage>{errors.passwordConfirm}</FormMessage>
+                )}
+              </FormItem>
+            )}
+          />
 
-        <Button
-          className={buttonStyles(
-            'btn-solid',
-            'btn-w-full',
-            'btn-px-lg',
-            'btn-py-md',
-            'btn-rounded-md'
+          {form.formState.errors.root && (
+            <p className="text-error">{form.formState.errors.root.message}</p>
           )}
-          type="submit"
-        >
-          Change password
-        </Button>
-      </form>
+
+          <Button
+            className={buttonStyles(
+              'btn-solid',
+              'btn-w-full',
+              'btn-px-lg',
+              'btn-py-md',
+              'btn-rounded-md'
+            )}
+            type="submit"
+            disabled={mutatePassword.isPending}
+          >
+            {mutatePassword.isPending ? 'Changing...' : 'Change Password'}
+          </Button>
+        </form>
+      </Form>
     </DialogContent>
   );
 }
