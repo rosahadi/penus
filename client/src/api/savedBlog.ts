daeb12f9d7b2ba716e2cff58aa2748034167f7de
rosa <rosahadi19@gmail.com>
2025-01-18 19:12:03 +0300

    Add client-side functions for saving, retrieving, and deleting saved blogs
    - Implement getSavedBlogs to fetch the list of saved blogs from the server.
    - Add saveBlog to send a request to save a blog for the authenticated user.
    - Add deleteSavedBlog to remove a saved blog from the user's saved list.
    - Handle errors using axios error handling with appropriate messages.

---
+++ client/src/api/savedBlog.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+export const getSavedBlogs = async () => {
+  try {
+    const res = await axios.get('/api/saveBlogs');
+    return res.data.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const errorData = error.response?.data;
+      throw errorData;
+    } else {
+      throw { message: 'An unexpected error occurred.' };
+    }
+  }
+};
+
+export const saveBlog = async (blogId: string | undefined) => {
+  try {
+    const res = await axios.post('/api/saveBlogs', { blog: blogId });
+    return res.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw error.response?.data.message;
+    } else {
+      throw { message: 'An unexpected error occurred.' };
+    }
+  }
+};
+
+export const deleteSavedBlog = async (id: string | undefined) => {
+  try {
+    const res = await axios.delete(`/api/saveBlogs/${id}`);
+    return res.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const errorData = error.response?.data;
+      throw errorData;
+    } else {
+      throw { message: 'An unexpected error occurred.' };
+    }
+  }
+};
